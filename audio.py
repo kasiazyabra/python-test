cc8cd8d99f08bc7b1d7929ed5c0e357d41253220
kasia <kasia.kasia@gmail.com>
2020-12-02 13:35:02 +0300

    def =>> class

--- audio.py
+++ audio.py
@@ -3,8 +3,8 @@ class Track:
     self.name = name
     self.duration = duration
 
-def show(Track):
-  print(f'<{Track.name} – {Track.duration}>')
+  def show(self):
+    print(f'<{self.name} – {self.duration}>')
 
 class Album:
   def __init__ (self, name, artist):
@@ -13,16 +13,16 @@ class Album:
     self.tracklist = []
     self.duration = 0
 
-def add_track(Album, track):
-  Album.tracklist.append(track)
-  Album.duration += track.duration
+  def add_track(self, track):
+    self.tracklist.append(track)
+    self.duration += track.duration
 
-def get_tracks(Album):
-  for track in Album.tracklist:
-    show(track)
+  def get_tracks(self):
+    for track in self.tracklist:
+      track.show()
 
-def get_duration(Album):
-  print(Album.duration)
+  def get_duration(self):
+    print(self.duration)
 
 
 track1 = Track('Тяжелый митал', 6)
@@ -31,14 +31,14 @@ track3 = Track('Пение всеумиленное для котиков', 777)
 album1 = Album('Первый альбом', 'Адская сотона')
 album2 = Album('Второй альбом', 'Православные песнопения')
 
-show(track1)
-show(track2)
-show(track3)
-add_track(album1,track1)
-add_track(album1,track2)
-add_track(album2,track3)
-get_tracks(album1)
-get_tracks(album2)
-get_duration(album1)
-get_duration(album2)
+track1.show()
+track2.show()
+track3.show()
+album1.add_track(track1)
+album1.add_track(track2)
+album2.add_track(track3)
+album1.get_tracks()
+album2.get_tracks()
+album1.get_duration()
+album2.get_duration()
 
