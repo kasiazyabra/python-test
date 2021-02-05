a391b800d465e70ae2d287d172cdeb76fbdf8f5b
kasia <kasia.kasia@gmail.com>
2021-02-05 19:01:09 +0300

    no range(3) now

--- cook_book.py
+++ cook_book.py
@@ -1,29 +1,32 @@
-def cook_book():
-  with open('recipes.txt') as f:
-    book = {}
-    for i in range(3):
-      dish = f.readline().strip()
-      ingnum = int(f.readline().strip())
-      ilist = []
-      for i in range(0,ingnum):
-        line = f.readline().strip().split(' | ')
-        idict = {}
-        idict.setdefault('ingredient_name', str(line[0]))
-        idict.setdefault('quantity', int(line[1]))
-        idict.setdefault('measure', str(line[2]))
-        ilist.append(idict)
-      book.setdefault(str(dish), ilist)
-      f.readline().strip()
-  return book
+def cook_book(filename):
+  cook_dict = {}
+  with open(filename, encoding='utf8') as file_work:
+
+    for line in file_work:
+      dish_name = line.strip()
+      counter = int(file_work.readline())
+      list_of_ingredient = []
+
+      for i in range(counter):
+        temp_dict = {}
+        ingredient = file_work.readline()
+        ingredient = ingredient.strip().split('|')
+        temp_dict['ingredient'] = ingredient[0].strip()
+        temp_dict['quantity'] = int(ingredient[1])
+        temp_dict['measure'] = ingredient[2].strip()
+        list_of_ingredient.append(temp_dict)
+
+      cook_dict[dish_name] = list_of_ingredient
+      file_work.readline()
+  return cook_dict
 
 def get_shop_list_by_dishes(dishes, person_count):
   shop_book = {}
   for dish in dishes:
-    for recipe, parts in cook_book().items():
+    for recipe, parts in cook_book('recipes.txt').items():
       if dish == recipe:
-        for ing in cook_book()[recipe]:
-          shop_book[ing['ingredient_name']] = {'measure': ing['measure'], 'quantity': ing['quantity'] * person_count}
-  print(shop_book)
+        for ing in cook_book('recipes.txt')[recipe]:
+          shop_book[ing['ingredient']] = {'measure': ing['measure'], 'quantity': ing['quantity'] * person_count}
+  return shop_book
 
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-
